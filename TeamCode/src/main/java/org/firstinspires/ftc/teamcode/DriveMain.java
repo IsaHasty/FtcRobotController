825b506896a16d7a0807170f12b3699376312f11
IsaHasty <145364434+IsaHasty@users.noreply.github.com>
2024-10-28 18:32:40 -0500

    Added arm and pincher to drivemain

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveMain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveMain.java
@@ -2,7 +2,9 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.CRServo;
 
 @TeleOp
 public class DriveMain extends LinearOpMode {
@@ -14,7 +16,8 @@ public class DriveMain extends LinearOpMode {
     private DcMotor backRight;
     private DcMotor elevator;
     private Servo pincher;
-    //private Servo pincher;
+    private DcMotor arm;
+    private CRServo intake;
 
     public void runOpMode() {
         //defines motors
@@ -25,6 +28,14 @@ public class DriveMain extends LinearOpMode {
         frontRight.setDirection(DcMotor.Direction.REVERSE);
         backRight.setDirection(DcMotor.Direction.REVERSE);
         pincher = hardwareMap.get(Servo.class, "pincher");
+        intake = hardwareMap.get(CRServo.class, "intake");
+
+        arm = hardwareMap.get(DcMotor.class, "arm");
+        arm.setDirection(DcMotor.Direction.REVERSE);
+        arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        arm.setTargetPosition(0);
+        arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
         elevator = hardwareMap.get(DcMotor.class, "elevator");
         elevator.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         elevator.setTargetPosition(0);
@@ -34,11 +45,12 @@ public class DriveMain extends LinearOpMode {
         pincher.setPosition(0.8);
         waitForStart();
 
-        double tgtPowerX = 0.0;
+
         double tgtPowerY = 0.0;
         double tgtPowerTurn = 0.0;
         double tgtPowerSide = 0.0;
         boolean isReversed = false;
+        boolean isIntakeTurning = false;
 
         while (opModeIsActive()){
             if(gamepad1.right_bumper){
@@ -46,7 +58,6 @@ public class DriveMain extends LinearOpMode {
                 sleep(300); //makes sure reverse isn't toggled multiple times
             }
             //driving
-            tgtPowerX = gamepad1.left_stick_x;
             tgtPowerY = gamepad1.left_stick_y;
             driveStraight(tgtPowerY, isReversed);
 
@@ -58,6 +69,8 @@ public class DriveMain extends LinearOpMode {
             //sideways
             tgtPowerSide = gamepad1.left_stick_x;
             driveSideways(tgtPowerSide, isReversed);
+
+
             if(gamepad1.x){
                 releaseElevator();
             }
@@ -73,6 +86,23 @@ public class DriveMain extends LinearOpMode {
                 numClicks++;
                 sleep(200);
                 changePincher(numClicks);
+            }
+
+            if(gamepad2.b){
+                isIntakeTurning = !isIntakeTurning;
+                sleep(200);
+                changeIntake(isIntakeTurning);
+            }
+
+            if(gamepad2.a){
+                armDown();
+            }
+
+            if(gamepad2.x){
+                armOut();
+            }
+            if(gamepad2.y){
+                armUp();
             }
         }
 
@@ -147,7 +177,31 @@ public class DriveMain extends LinearOpMode {
         else{
             pincher.setPosition(.3);
         }
+    }
 
+    public void changeIntake(boolean isIntakeTurning){
+        if(isIntakeTurning){
+            intake.setPower(-1);
+        }
+        else{
+            intake.setPower(0);
+        }
+    }
+
+    public void armUp(){
+        arm.setPower(0.5);
+        arm.setTargetPosition(2600);
+
+    }
+
+    public void armDown(){
+        arm.setPower(0.5);
+        arm.setTargetPosition(250);
+    }
+
+    public void armOut(){
+        arm.setPower(0.5);
+        arm.setTargetPosition(500);
     }
 }
 //things i could do: make an array of motors
